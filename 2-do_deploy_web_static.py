09eb5a23eab84f347c7d76e21d5dc147bf4f0904
Stephanie <Stephaniechloe21@gmail.com>
2023-09-08 16:58:12 -0700

    task 1 works guy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """A module for web application deployment with Fabric."""
 import os
-from datetime import datetime
-from fabric.api import run, env, put, local
+from fabric.api import run, env, put
 
 env.hosts = ['54.144.128.232', '18.204.20.190']
 
@@ -10,12 +9,12 @@ env.hosts = ['54.144.128.232', '18.204.20.190']
 def do_deploy(archive_path):
     """Deploys the static files to the host servers."""
 
-    if os.path.exists(archive_path) is False:
+    if not os.path.exists(archive_path):
         return False
     filename = archive_path.split('/')[1]
     filepath = f"/data/web_static/releases/{filename.replace('.tgz', '')}/"
     try:
-        put(archive_path, f"/tmp/{filename}")
+        put(archive_path, "/tmp/")
         run(f"mkdir -p {filepath}")
         run(f"tar -xzf /tmp/{filename} -C {filepath}")
         run(f"rm /tmp/{filename}")
@@ -23,8 +22,6 @@ def do_deploy(archive_path):
         run(f"rm -rf {filepath}web_static")
         run(f"rm -rf /data/web_static/current")
         run(f"ln -s {filepath} /data/web_static/current")
-        print('New version deployed!')
-        success = True
+        return True
     except Exception:
-        success = False
-    return success
+        return False
