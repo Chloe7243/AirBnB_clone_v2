d54cddbf53afb5605865afaa1f004b4c837f5554
Stephanie <Stephaniechloe21@gmail.com>
2023-09-08 15:45:35 -0700

    task 0?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,11 @@ fi
 sudo mkdir -p "/data/web_static/releases/test" "/data/web_static/shared"
 bash -c "echo Holberton School > /data/web_static/releases/test/index.html"
 sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo chown -hR ubuntu:ubuntu "/data"
 
 static_block="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
-if [[ "$(grep -c 'location /hbnb_static {' '/etc/nginx/sites-available/default')" < '1' ]]; then
+if [[ "$(grep -c 'location /hbnb_static {' '/etc/nginx/sites-available/default')" -lt '1' ]]; then
     sudo sed -i "/error_page 404 /a${static_block}" "/etc/nginx/sites-available/default"
 fi
 sudo service nginx restart
