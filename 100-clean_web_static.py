da4cd74e29708a99df0998267ca9a95cd67436c2
Stephanie <Stephaniechloe21@gmail.com>
2023-09-07 21:20:42 -0700

    tasks

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,13 +9,13 @@ env.hosts = ['54.144.128.232', '18.204.20.190']
 
 def do_clean(number=0):
     """Deletes files from directory"""
-    
+
     archives = os.listdir('versions/')
     archives.sort(reverse=True)
     try:
         for archive in archives[int(number):]:
             local(f"rm versions/{archive}")
-            run(f"rm /data/web_static/releases/{archive}") 
+            run(f"rm /data/web_static/releases/{archive}")
         success = True
     except Exception:
         success = False
