bcfbccaa24ef31f0e97cf66a4e56203f378da6e5
Stephanie <Stephaniechloe21@gmail.com>
2023-09-07 19:22:31 -0700

    tired

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,7 @@
 """A module for web application deployment with Fabric."""
 import os
 from datetime import datetime
-from fabric.api import run, env, put, local, runs_once
+from fabric.api import run, env, put, local
 
 env.hosts = ['54.144.128.232', '18.204.20.190']
 
@@ -13,15 +13,16 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     filename = archive_path.split('/')[1]
-    filepath = f"/data/web_static/releases/{filename.replace('.tgz', '')}"
+    filepath = f"/data/web_static/releases/{filename.replace('.tgz', '')}/"
     try:
         put(archive_path, f"/tmp/{filename}")
         run(f"mkdir -p {filepath}")
-        run(f"tar -xzf /tmp/{filename} -C {filepath}/")
-        run(f"mv {filepath}/web_static/* {filepath}/")
-        run(f"rm -rf {filepath}/web_static")
+        run(f"tar -xzf /tmp/{filename} -C {filepath}")
+        run(f"rm /tmp/{filename}")
+        run(f"mv {filepath}web_static/* {filepath}")
+        run(f"rm -rf {filepath}web_static")
         run(f"rm -rf /data/web_static/current")
-        run(f"ln -s {filepath}/ /data/web_static/current")
+        run(f"ln -s {filepath} /data/web_static/current")
         print('New version deployed!')
         success = True
     except Exception:
