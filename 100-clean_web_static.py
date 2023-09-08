8719a35d468841610e5d301e075ee5fd50cb4575
Stephanie <Stephaniechloe21@gmail.com>
2023-09-07 21:11:46 -0700

    i tried

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import run, env, put, local
+
+env.hosts = ['54.144.128.232', '18.204.20.190']
+
+
+def do_clean(number=0):
+    """Deletes files from directory"""
+    
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    try:
+        for archive in archives[int(number):]:
+            local(f"rm versions/{archive}")
+            run(f"rm /data/web_static/releases/{archive}") 
+        success = True
+    except Exception:
+        success = False
+    return success
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""A module for web application deployment with Fabric."""
+import os
+from datetime import datetime
+from fabric.api import run, env, put, local, runs_once
+
+env.hosts = ['54.144.128.232', '18.204.20.190']
+
+
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers."""
+
+    if not os.path.exists(archive_path):
+        return False
+    filename = archive_path.split('/')[1]
+    filepath = f"/data/web_static/releases/{filename.replace('.tgz', '')}/"
+    try:
+        put(archive_path, f"/tmp/{filename}")
+        run(f"mkdir -p {filepath}")
+        run(f"tar -xzf /tmp/{filename} -C {filepath}")
+        run(f"rm -rf /tmp/{filename}")
+        run(f"mv {filepath}web_static/* {filepath}")
+        run(f"rm -rf {filepath}web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {filepath} /data/web_static/current")
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
+
+
+def deploy():
+    """Archives and deploys the static files to the host servers.
+    """
+    acp = do_pack()
+    print("acp", acp)
+    return do_deploy(acp)
