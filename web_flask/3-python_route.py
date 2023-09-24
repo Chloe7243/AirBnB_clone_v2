d37e2cb38ca234b5155680f03d5a5a889c21e3ab
Stephanie <Stephaniechloe21@gmail.com>
2023-09-24 13:47:25 -0700

    flask 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Web Flask """
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="python"):
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
