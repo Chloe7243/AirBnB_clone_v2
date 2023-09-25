d916163cfb673c7b4aa3b43f0176c1203f14ab5a
Stephanie <Stephaniechloe21@gmail.com>
2023-09-24 21:19:04 -0700

    10

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Web Flask """
+from models import storage
+from flask import Flask, request, render_template
+from models.state import State
+from models.city import City
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.teardown_appcontext
+def teardown_db(e):
+    storage.close()
+
+
+@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/', strict_slashes=False)
+def states(id=None):
+    case = 1
+    states = list(storage.all(State).values())
+    state = None
+    if id is not None:
+        case = 2
+        for state in states:
+            if state.id == id:
+                state = state
+                break
+    return render_template('9-states.html', case=case, states=states, state=state)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
